3902ad02edd3c7f536d2e0674eddcc441be5cafd
SWD <116519976+sabrinawdesmond@users.noreply.github.com>
2023-03-29 14:16:16 -0400

    redux store setup

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
-import { createStore, combineReducers, applyMiddleware, compose} from 'redux'
-import { thunk } from 'redux-thunk'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
 
 const rootReducer = combineReducers({
 
